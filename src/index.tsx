ac843e4a4292280528ecdf1f5f90f2b3241879df
Ganyponko <ni@yandex.ru>
2020-09-01 10:49:03 +0400

    add redux provider for app;

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,15 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import "./index.css"
-import App from "./app/App"
-import * as serviceWorker from "./serviceWorker"
-ReactDOM.render(<App />, document.getElementById("root"))
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import { Provider } from 'react-redux'
+import App from './app/App'
+import store from './store/store'
+import * as serviceWorker from './serviceWorker'
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+)
 
 serviceWorker.unregister()
