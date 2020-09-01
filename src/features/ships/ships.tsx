f7ebbc4bc29d1ce4cd5df6368c4474df930137ac
Nikita <nik.wiop@yandex.ru>
2020-09-01 23:49:07 +0400

    add feat for ships ;

--- src/features/ships/ships.tsx
+++ src/features/ships/ships.tsx
@@ -1,5 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { RootState } from '../../store/root-reducer'
+import { fetchShips, fetchShipById } from '../feats-thunk'
+import Content from '../../components/content'
+import Sider from '../../components/sider'
 
-const Ships = () => <div>Ships</div>
+const Ships = () => {
+  const dispatch = useDispatch()
+  const { listData, contentData } = useSelector(
+    (state: RootState) => state.feats,
+  )
+
+  useEffect(() => {
+    dispatch(fetchShips())
+  })
+
+  const planetsData = listData.map((item) => {
+    const id = item.url.match(/\/(\d+?)\//)[1]
+    return {
+      title: item.name,
+      id: id,
+      onItemClick: () => dispatch(fetchShipById(id)),
+    }
+  })
+
+  return (
+    <>
+      <Sider listData={planetsData} />
+      <Content img={contentData.img} description={contentData.description} />
+    </>
+  )
+}
 
 export default Ships
