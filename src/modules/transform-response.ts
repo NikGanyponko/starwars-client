2f9820843b299ee5f8530314d1c3a60ee37e5c33
Ganyponko <ni@yandex.ru>
2020-09-02 11:15:53 +0400

    del console.log ;

--- src/modules/transform-response.ts
+++ src/modules/transform-response.ts
@@ -40,9 +40,6 @@ export const transformShipResponse = (res: any) => {
 
 export const transformPersonResponse = (res: any) => {
   const id = res.url.match(/\/(\d+?)\//)[1]
-
-  console.log('!!!! ', res)
-
   return {
     description: [
       res.name,
