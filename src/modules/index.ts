941d98fc6f526e5d969c6c14822959d060c253c0
Nikita <nik.wiop@yandex.ru>
2020-09-07 23:03:33 +0400

    add locale and update modules imports ;

---
+++ src/modules/index.ts
@@ -0,0 +1,17 @@
+import api from './api-client'
+import {
+  matchId,
+  transformPlanetResponse,
+  transformShipResponse,
+  transformPersonResponse,
+} from './transform-response'
+import locale from './locale/en.json'
+
+export {
+  api,
+  locale,
+  matchId,
+  transformPlanetResponse,
+  transformShipResponse,
+  transformPersonResponse,
+}
